75c1d947a9e808aa7066ac4a9299a67985ff5aa8
Beshoy Shehata <0xbeshoy@gmail.com>
2022-04-05 16:56:27 +0200

    ..

---
+++ SolUtils/contracts/MathUtils.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.8;
+
+contract MathUtils {
+    function add(uint256 a1, uint256 b1) public pure returns (uint256) {
+        return (a1 + b1);
+    }
+
+    function sub(uint256 a1, uint256 b1) public pure returns (uint256) {
+        return (a1 - b1);
+    }
+
+    function mult(uint256 a1, uint256 b1) public pure returns (uint256) {
+        return (a1 * b1);
+    }
+
+    function div(uint256 a1, uint256 b1) public pure returns (uint256) {
+        return (a1 * b1);
+    }
+}
